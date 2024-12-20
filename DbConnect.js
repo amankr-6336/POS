12e143bccd0d7380feb37496d8596211dcaf2a34
amankr.6336 <amankr.17709418@gmail.com>
2024-12-20 20:37:35 +0530

    project setup and mongodb connected

---
+++ DbConnect.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+mongoose.set('strictQuery',true);
+const { ServerApiVersion } = require('mongodb');
+
+
+module.exports=async ()=>{
+    const mongoURI="mongodb+srv://amankr63366:Callofduty@clusterpos.qhucb.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPOS"
+
+    try {
+        const connect=await mongoose.connect(mongoURI, {serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+          }});
+
+        console.log(`mongodb connected : ${connect.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
